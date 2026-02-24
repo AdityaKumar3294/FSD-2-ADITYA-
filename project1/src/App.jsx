efa2813d0e3bbb8dbe4937015b7b488458a3f9dc
adityakumar <adityabrn2004@gmail.com>
2026-02-24 13:01:50 +0530

    Initial commit

---
+++ project1/src/App.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./Welcome";
+function App() {
+  return (
+    <div>
+      <h1>Main App Component</h1>
+      <Welcome>
+        <p>This is the main app component.</p>
+      </Welcome>
+    </div>
+  )
+}
